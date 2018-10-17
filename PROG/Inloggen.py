2244b945d83cc56d6a4b8cc8b8a5946d6a483896
Nic Jenneboer <nic.jenneboer@student.hu.nl>
2018-10-17 21:45:46 +0200

    Inlog systeem met database en encrypted wachtwoord

--- PROG/Inloggen.py
+++ PROG/Inloggen.py
@@ -1,7 +1,37 @@
 import sqlite3
-con = sqlite3.connect('film.db')
-cur = con.cursor()
 import hashlib
+import requests
+import datetime
+import xml.etree.ElementTree as ET
+import xmltodict
+import sqlite3
+
+
+def api_ophalen(dag):
+    # dag = 0: Vandaag
+    # dag = 1: Morgen
+    key = "z9rqxl4qlkw14ozm3z5721oscmu88zoz"
+    datenow = datetime.datetime.now()
+    if dag == 0:
+        date = datenow.strftime("%d-%m-%Y")
+    elif dag == 1:
+        date = str(int(datenow.strftime("%d")) + 1) + datenow.strftime("-%m-%Y")
+    sorteer = "0"
+    api_url = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey=' + key + '&dag=' + date + '&sorteer=' + sorteer
+    response = requests.get(api_url)
+    cur.execute('SELECT titel FROM Films')
+    titels = cur.fetchall()
+    columns = ['titel', 'jaar', 'regisseur', 'cast', 'genre', 'land', 'cover', 'duur', 'synopsis', 'imdb_rating', 'starttijd', 'eindtijd', 'filmtip']
+    gegevens = xmltodict.parse(response.text)
+    field = []
+    for i in range(len(gegevens["filmsoptv"]["film"])):
+        for column in columns:
+            field += [(gegevens["filmsoptv"]["film"][i][column])]
+        field += ['0'] + [date]
+        if ((field[0],) not in titels):
+            cur.execute("INSERT INTO Films VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", field)
+            con.commit()
+        field = []
 
 def create_users(auth):
     naam = input("Naam: ")
@@ -18,6 +48,7 @@ def create_users(auth):
 
 
 def inloggen():
+    global naam
     naam = input("Naam: ")
     ww = input("Wachtwoord: ")
     wrong = False
@@ -28,9 +59,9 @@ def inloggen():
             if (cur.fetchall()[0][0]) == hashlib.sha256(ww.encode()).hexdigest():
                 cur.execute("SELECT auth FROM Users WHERE naam = ?", (naam,))
                 if cur.fetchall()[0][0] == 0:
-                    print("Welkom gebruiker!")
+                    gebruiker()
                 else:
-                    print("Welkom aanbieder!")
+                    aanbieder()
                 wrong = False
             else:
                 wrong = True
@@ -39,16 +70,50 @@ def inloggen():
     if wrong == True:
         print ("Verkeerde gegevens!")
 
+def aanbieder():
+    print("Welkom aanbieder!")
+    print("\n")
+    menu = int(input("1. Beschibare films om aan te bieden.\n2. Mijn Films\n\n>>"))
+    if menu == 1:
+        print("Films Beschikbaar om aan te bieden:\n\n")
+        cur.execute('SELECT titel FROM Films WHERE aanbieder = "0"')
+        number = 0
+        titels = cur.fetchall()
+        for titel in titels:
+            number += 1
+            print("{}. {} ".format(number, titel[0]))
+        q = int(input("Welke Film wil je zelf aanbieden?: "))
+        print (titels[q - 1][0])
+        cur.execute('UPDATE Films SET aanbieder = ? WHERE titel = ?', (naam, titels[q - 1][0]))
+        con.commit()
+    elif menu == 2:
+        cur.execute('SELECT titel FROM Films WHERE aanbieder = ?', (naam,))
+        for titels in cur.fetchall():
+            print(titels[0])
+def gebruiker():
+    print("Welkom aanbieder!")
+    print("\n")
+    menu = int(input("1. Beschibare films om te bekijken.\n2. Mijn Films\n\n>>"))
+    if menu == 1:
+        cur.execute('SELECT titel FROM Films WHERE NOT aanbieder = "0"')
+        for titels in cur.fetchall():
+            print(titels[0])
+
+con = sqlite3.connect('film.db')
+cur = con.cursor()
+
+api_ophalen(0)
+api_ophalen(1)
 
 
+x = int(input("1. Inloggen\n2. Account aanmaken\n3. Stoppen\n\n>> "))
+if x == 1:
+    inloggen()
+elif x == 2:
+    auth = int(input("0. Gebruikers account\n1. Aanbieders account\n\n>> "))
+    create_users(auth)
+else:
+    exit()
 
-while True:
-    x = int(input("1. Inloggen\n2. Account aanmaken\n3. Stoppen\n\n>> "))
-    if x == 1:
-        inloggen()
-    elif x == 2:
-        auth = int(input("0. Gebruikers account\n1. Aanbieders account\n\n>> "))
-        create_users(auth)
-    else:
-        exit()
+
 con.close()
--- PROG/main.py
+++ PROG/main.py
@@ -4,6 +4,9 @@ import xml.etree.ElementTree as ET
 import xmltodict
 import sqlite3
 
+
+
+
 def api_ophalen(dag):
     # dag = 0: Vandaag
     # dag = 1: Morgen
@@ -15,14 +18,24 @@ def api_ophalen(dag):
         date = str(int(datenow.strftime("%d")) + 1) + datenow.strftime("-%m-%Y")
     sorteer = "0"
     api_url = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey=' + key + '&dag=' + date + '&sorteer=' + sorteer
-    return requests.get(api_url)
+    response = requests.get(api_url)
+    cur.execute('SELECT titel FROM Films')
+    titels = cur.fetchall()
+    columns = ['titel', 'jaar', 'regisseur', 'cast', 'genre', 'land', 'cover', 'duur', 'synopsis', 'imdb_rating', 'starttijd', 'eindtijd', 'filmtip']
+    gegevens = xmltodict.parse(response.text)
+    field = []
+    for i in range(len(gegevens["filmsoptv"]["film"])):
+        for column in columns:
+            field += [(gegevens["filmsoptv"]["film"][i][column])]
+        field += ['0'] + [date]
+        if ((field[0],) not in titels):
+            cur.execute("INSERT INTO Films VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", field)
+            con.commit()
+        field = []
 
-def api_gegevens(gegeven):
-    gegevens = xmltodict.parse(api_ophalen(0).text)
-    vraag = []
-    for i in gegevens["filmsoptv"]["film"]:
-        vraag += [i[gegeven]]
-    return vraag
 
-
-print (api_gegevens("jaar"))
+con = sqlite3.connect('film.db')
+cur = con.cursor()
+api_ophalen(0)
+api_ophalen(1)
+con.close()
