f41b99b81bbebbad6e57be0cda3ea748e6344a7e
Nic Jenneboer <nic.jenneboer@student.hu.nl>
2018-10-25 19:51:14 +0200

    25-10-18

--- PROG/GUI.py
+++ PROG/GUI.py
@@ -7,6 +7,7 @@ import hashlib
 from urllib.request import urlopen
 from PIL import Image, ImageTk
 import io
+from tkinter.messagebox import showinfo
 
 
 #API TO DATABASE FUNCITES
@@ -53,31 +54,30 @@ def api_to_database():
 
 
 #toon Frames Functies
+def toonFilmsAanbiederFrame():
+    loginframe.pack_forget()
+    mijnfilmsaanbiederframe.pack_forget()
+    filmsaanbiederframe.pack()
+    Filmsaanbiederscreen()
 
+def toonMijnFilmsAanbiederFrame():
+    mijnfilmsaanbiederframe.pack
+    filmsaanbiederframe.pack_forget()
 
 def toonLoginFrame():
     gebruikerhoofdframe.pack_forget()
-    aanbiederhoofdframe.pack_forget()
     registerframe.pack_forget()
     loginframe.pack()
     Loginscreen()
 
 def toonGebruikerHoofdFrame():
-    aanbiederhoofdframe.pack_forget()
     loginframe.pack_forget()
     registerframe.pack_forget()
     gebruikerhoofdframe.pack()
 
-def toonAanbiederHoofdFrame():
-    loginframe.pack_forget()
-    gebruikerhoofdframe.pack_forget()
-    registerframe.pack_forget()
-    aanbiederhoofdframe.pack()
-
 def toonRegisterFrame():
     loginframe.pack_forget()
     gebruikerhoofdframe.pack_forget()
-    aanbiederhoofdframe.pack_forget()
     registerframe.pack()
     Registerscreen()
 
@@ -98,18 +98,19 @@ def login_clicked(usern, userp):
                 if cur.fetchall()[0][0] == 0:
                     gebruiker_login()
                 else:
-                    aanbieder_login()
+                    toonFilmsAanbiederFrame()
                 wrong = False
             else:
                 wrong = True
         else:
             wrong = True
     if wrong == True:
-        wrong_input_login.config(text='Verkeerde gegevens!')
+        popup('Verkeerde inlog gegevens!')
 
 
 
 def registreer_clicked(auth, usern, userp, userpc):
+    gebruikersnaambestaatniet = False
     naam = usern
     autho = auth
     ww = hashlib.sha256(userp.encode()).hexdigest()
@@ -117,22 +118,32 @@ def registreer_clicked(auth, usern, userp, userpc):
         cur.execute('SELECT * FROM Users')
         for i in cur:
             if naam == i[0]:
-                print ("Naam bestaad al!")
-                exit()
-        record = (naam, ww, autho)
-        print (record)
-        cur.execute("INSERT INTO Users VALUES (?, ?, ?)", record)
-        con.commit()
-        toonLoginFrame()
-    else:
-        print ("Fout")
+                gebruikersnaambestaatniet = False
+                break
+            else:
+                gebruikersnaambestaatniet = True
+        if gebruikersnaambestaatniet == True:
+            record = (naam, ww, autho)
+            cur.execute("INSERT INTO Users VALUES (?, ?, ?)", record)
+            con.commit()
+            if autho == '0':
+                popup('Gebruikers account aangemaakt!')
+            elif autho == '1':
+                popup('Aanbieders account aangemaakt!')
+            toonLoginFrame()
+        elif gebruikersnaambestaatniet == False:
+            popup('Gebruikersnaam bestaat al')
 
+    else:
+        popup('Wachtwoord komt niet overheen')
 #GUI FRAMES
 
+def popup(bericht):
+    showinfo(title='popup', message=bericht)
 
 def Loginscreen():
     global wrong_input_login
-    BACKGROUND(loginframe, 'img/background.png')
+    BACKGROUND(loginframe, 'img/loginbackground.png')
     username_entry = Entry(master=loginframe, width=25, font=('Verdana', 15))
     userpass_entry = Entry(master=loginframe, width=25, font=('Verdana', 15), show="*")
     username_label = Label(master=loginframe, text="Gebruikersnaam:", font=('Verdana', 15))
@@ -142,10 +153,10 @@ def Loginscreen():
     wrong_input_login = Label(master=loginframe, text="", font=('Verdana', 15))
     # Place
     username_label.place(relx=0.3, rely=0.58, anchor=CENTER)
-    userpass_label.place(relx=0.3, rely=0.63, anchor=CENTER)
-    username_entry.place(relx=0.5, rely=0.58, anchor=CENTER)
-    userpass_entry.place(relx=0.5, rely=0.63, anchor=CENTER)
-    login_button.place(relx=0.68, rely=0.605, anchor=CENTER)
+    userpass_label.place(relx=0.315, rely=0.63, anchor=CENTER)
+    username_entry.place(relx=0.52, rely=0.58, anchor=CENTER)
+    userpass_entry.place(relx=0.52, rely=0.63, anchor=CENTER)
+    login_button.place(relx=0.70, rely=0.605, anchor=CENTER)
     registreerframe_button.place(relx=0.85, rely=0.05, anchor=CENTER)
     wrong_input_login.place(relx=0.5, rely=0.53, anchor=CENTER)
 
@@ -153,10 +164,10 @@ def Loginscreen():
 
 
 def Registerscreen():
-    BACKGROUND(registerframe, 'img/background.png')
+    BACKGROUND(registerframe, 'img/registreerbackground.png')
     v = IntVar()
-    aanbieder = Radiobutton(registerframe, text="Aanbieder", variable=v, value=1)
-    gebruiker = Radiobutton(registerframe, text="Gebruiker", variable=v, value=0)
+    aanbieder = Radiobutton(registerframe, text="Aanbieder", variable=v, value=1,font=('Verdana', 12))
+    gebruiker = Radiobutton(registerframe, text="Gebruiker", variable=v, value=0,font=('Verdana', 12))
     gebruiker.invoke()
     username_entry = Entry(master=registerframe, width=25, font=('Verdana', 15))
     userpass_entry = Entry(master=registerframe, width=25, font=('Verdana', 15), show="*")
@@ -167,16 +178,24 @@ def Registerscreen():
     registreer_button = Button(master=registerframe, text="Registreer!",command=lambda: registreer_clicked(str(v.get()), username_entry.get(), userpass_entry.get(), userpasscheck_entry.get()),font=('Verdana', 20), activebackground="#b00000", background="#d00000", bd=0, fg="#fff")
     loginframe_button = Button(master=registerframe, text="Login",command=lambda: toonLoginFrame(),font=('Verdana', 20), activebackground="#ffcccc", background="#fff", bd=0, fg="#b00000",border=0)
     # Place
-    aanbieder.place(relx=0.55, rely=0.54, anchor=CENTER)
-    gebruiker.place(relx=0.46, rely=0.54, anchor=CENTER)
-    username_label.place(relx=0.335, rely=0.58, anchor=CENTER)
-    userpass_label.place(relx=0.35, rely=0.63, anchor=CENTER)
-    userpasscheck_label.place(relx=0.31, rely=0.68, anchor=CENTER)
-    username_entry.place(relx=0.55, rely=0.58, anchor=CENTER)
-    userpass_entry.place(relx=0.55, rely=0.63, anchor=CENTER)
-    userpasscheck_entry.place(relx=0.55, rely=0.68, anchor=CENTER)
+    aanbieder.place(relx=0.60, rely=0.528, anchor=CENTER)
+    gebruiker.place(relx=0.51, rely=0.528, anchor=CENTER)
+    username_label.place(relx=0.402, rely=0.58, anchor=CENTER)
+    userpass_label.place(relx=0.419, rely=0.63, anchor=CENTER)
+    userpasscheck_label.place(relx=0.38, rely=0.68, anchor=CENTER)
+    username_entry.place(relx=0.6, rely=0.58, anchor=CENTER)
+    userpass_entry.place(relx=0.6, rely=0.63, anchor=CENTER)
+    userpasscheck_entry.place(relx=0.6, rely=0.68, anchor=CENTER)
     registreer_button.place(relx=0.61, rely=0.75, anchor=CENTER)
     loginframe_button.place(relx=0.94, rely=0.05, anchor=CENTER)
+
+def Filmsaanbiederscreen():
+    BACKGROUND(filmsaanbiederframe, 'img/films-a-background.png')
+    mijnfilms_button = Button(master=filmsaanbiederframe, text='Mijn Films', width=10, height=2,font=('Verdana', 14), activebackground="#ffcccc", background="#fff", bd=0, fg="#b00000",border=0)
+    mijnfilms_button.place(relx=0.232, rely=0.24, anchor=CENTER)
+
+
+
 
 def gebruiker_login():
     toonGebruikerHoofdFrame()
@@ -185,55 +204,6 @@ def gebruiker_login():
     b = Button(master=gebruikerhoofdframe, command=lambda: toonLoginFrame(), width=12, height=12)
     b.pack(pady=10)
 
-def sjaak(p):
-    global tk_img
-    global tk_img2
-    global tk_img3
-    global x
-    if p == 1:
-        x = x + 1
-    else:
-        x = x - 1
-    print (x)
-    for i in range(3):
-        url = (list[i + x])
-        print (list[i + x])
-        my_page = urlopen(url)
-        my_picture = io.BytesIO(my_page.read())
-        pil_img = Image.open(my_picture)
-        if i == 0:
-            tk_img = ImageTk.PhotoImage(pil_img)
-            label = Button(master=aanbiederhoofdframe, image=tk_img)
-            label.place(relx=0.2, rely=0.5, anchor=CENTER)
-        elif i == 1:
-            tk_img2 = ImageTk.PhotoImage(pil_img)
-            label2 = Button(master=aanbiederhoofdframe, image=tk_img2)
-            label2.place(relx=0.5, rely=0.5, anchor=CENTER)
-        elif i == 2:
-            tk_img3 = ImageTk.PhotoImage(pil_img)
-            label3 = Button(master=aanbiederhoofdframe, image=tk_img3)
-            label3.place(relx=0.8, rely=0.5, anchor=CENTER)
-
-def aanbieder_login():
-    global tk_img
-    global tk_img2
-    global tk_img3
-    global list
-    global x
-    BACKGROUND(aanbiederhoofdframe, 'img/background.png')
-    toonAanbiederHoofdFrame()
-    cur.execute('SELECT cover FROM Films WHERE aanbieder = "0"')
-    titels = cur.fetchall()
-    list = []
-    for titel in titels:
-        list += [str(titel[0])]
-    print(list)
-    x = 0
-    sjaak(1)
-    b = Button(master=aanbiederhoofdframe, command=lambda: sjaak(2), width=5, height=5, text="<")
-    b.place(relx=0.05, rely=0.5, anchor=CENTER)
-    c = Button(master=aanbiederhoofdframe, command=lambda: sjaak(1), width=5, height=5, text=">")
-    c.place(relx=0.95, rely=0.5, anchor=CENTER)
 
 
 def BACKGROUND(frame, img):
@@ -268,15 +238,15 @@ root.resizable(False, False)
 #Frames
 loginframe = Frame(master=root)
 gebruikerhoofdframe = Frame(master=root)
-aanbiederhoofdframe = Frame(master=root)
 registerframe = Frame(master=root)
+filmsaanbiederframe = Frame(master=root)
+mijnfilmsaanbiederframe = Frame(master=root)
 
 
 
 
 
-Loginscreen()
-toonLoginFrame()
+toonFilmsAanbiederFrame()
 
 
 root.mainloop()
